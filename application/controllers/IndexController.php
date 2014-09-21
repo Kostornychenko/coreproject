ef9bb54be158a78604d1c56b5aef29b29b39ff0d
Kostornychenko <kostornychenko@gmail.com>
2014-09-21 23:20:47 +0300

    version 1.0

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -14,36 +14,39 @@ class IndexController extends Zend_Controller_Action {
         require_once 'Zend/Loader.php';
         Zend_Loader::loadClass('Zend_Http_Client');
 
-        // определение ключа и секретного ключа пользователя
-        // доступно с консоли API Instagram
         $CLIENT_ID = 'fa2641332bca4ce6bd8f71bd4345cc0f';
         $CLIENT_SECRET = 'c44e29e7449444e3bc1f58ff499f5e8e';
-        $tag = '332756956';
-        $tag1 = 'coreproject';
+        $user = '332756956';
+        $tag = 'coreproject';
 
         try {
-            // initialize client
-            $client = new Zend_Http_Client('https://api.instagram.com/v1/users/'.
-                $tag . '/media/recent');
+            $client = new Zend_Http_Client('https://api.instagram.com/v1/users/'.$user.'/media/recent');
             $client->setParameterGet('client_id', $CLIENT_ID);
-
-            // получение изображений с соответствующими метками
-            // передача запроса и декодирование ответа
             $response = $client->request();
             $result = json_decode($response->getBody());
-
-            // отображение фотографий
             $data = $result->data;
             if (count($data) > 0) {
                 $this->view->user = $data;
+            }
+        } catch (Exception $e) {
+            echo 'ERROR: ' . $e->getMessage() . print_r($client);
+            exit;
+        }
 
+        try {
+            $client = new Zend_Http_Client('https://api.instagram.com/v1/tags/'.$tag.'/media/recent');
+            $client->setParameterGet('client_id', $CLIENT_ID);
+            $response = $client->request();
+            $result = json_decode($response->getBody());
+            $data = $result->data;
+            if (count($data) > 0) {
+                $this->view->tag = $data;
             }
 
         } catch (Exception $e) {
             echo 'ERROR: ' . $e->getMessage() . print_r($client);
             exit;
         }
-
     }
 }
 
